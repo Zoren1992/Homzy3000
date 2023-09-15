71e760d542bcd95aaaeac06c3a81f6c277bcc799
Zoren1992 <zor.htataung1992@gmail.com>
2023-09-15 12:59:16 +1000

    second commit

---
+++ client/src/utils/accordion.jsx
@@ -0,0 +1,23 @@
+import { HiShieldCheck } from "react-icons/hi";
+import { MdCancel, MdAnalytics } from "react-icons/md";
+const data = [
+  {
+    icon: <HiShieldCheck/>,
+    heading: "Best interest rates on the market",
+    detail:
+      "Stability is the cornerstone of wise real estate investments. We employ meticulous market analysis and expert guidance to help you navigate the ever-changing real estate landscape. Our goal is to safeguard your investment from the uncertainties of fluctuating prices, ensuring your peace of mind and long-term financial security.",
+  },
+  {
+    icon: <MdCancel />,
+    heading: "Prevent unstable prices",
+    detail:
+      "Count on us to safeguard your investment against price volatility, ensuring a stable and secure real estate journey",
+  },
+  {
+    icon: <MdAnalytics />,
+    heading: "Best price on the market",
+    detail:
+      "Discover your ideal property at the best prices on the market. Let us guide you to unmatched value and unbeatable deals",
+  },
+];
+export default data;
